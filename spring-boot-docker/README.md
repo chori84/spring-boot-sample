3b31c420ac22da33c237a96f48536d3f7898369c
chori <slivers721@gmail.com>
2018-04-22 22:06:38 +0900

    Spring Boot with Docker

---
+++ spring-boot-docker/README.md
@@ -0,0 +1,8 @@
+# Spring Boot with Docker
+
+https://spring.io/guides/gs/spring-boot-docker/
+
+```
+./mvnw install dockerfile:build
+docker run -p 8088:8080 -t chori/spring-boot-docker
+```
---
+++ spring-boot-docker/src/main/java/com/chori/springbootdocker/SpringBootDockerApplication.java
@@ -0,0 +1,20 @@
+package com.chori.springbootdocker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SpringBootDockerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootDockerApplication.class, args);
+    }
+
+    @GetMapping("/")
+    public String home() {
+        return "Hello Docker!";
+    }
+}
